068d1a0509720b86e6dbef83a29af28771f840be
Utkarsh <ignition3951@gmail.com>
2024-11-26 16:04:40 +0530

    initial commit

---
+++ src/test/java/com/utk/SiaWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiaWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
