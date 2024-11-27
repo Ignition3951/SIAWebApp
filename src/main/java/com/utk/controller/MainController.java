6f20ded9b3b0787e3225f66fae6818d86ab65500
Utkarsh <ignition3951@gmail.com>
2024-11-27 17:18:36 +0530

    Implement path variable

--- src/main/java/com/utk/controller/MainController.java
+++ src/main/java/com/utk/controller/MainController.java
@@ -2,6 +2,7 @@ package com.utk.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,6 +13,13 @@ public class MainController {
 	public String home(Model page, @RequestParam String randomName, @RequestParam String randomColor) {
 		page.addAttribute("username", randomName);
 		page.addAttribute("color", randomColor);
+		return "home.html";
+	}
+
+	@RequestMapping("/home/{color}")
+	public String homeWithPathVariable(Model page, @PathVariable String color) {
+		page.addAttribute("username", "Utkarsh");
+		page.addAttribute("color", color);
 		return "home.html";
 	}
 
