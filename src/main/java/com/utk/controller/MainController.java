35ad777668af58d733f9fe07dc29d8128439ea84
Utkarsh <ignition3951@gmail.com>
2024-11-27 17:10:46 +0530

    Implement request paarameters in controller

--- src/main/java/com/utk/controller/MainController.java
+++ src/main/java/com/utk/controller/MainController.java
@@ -3,14 +3,15 @@ package com.utk.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
 	@RequestMapping("/home")
-	public String home(Model page) {
-		page.addAttribute("username", "Utkarsh");
-		page.addAttribute("color", "blue");
+	public String home(Model page, @RequestParam String randomName, @RequestParam String randomColor) {
+		page.addAttribute("username", randomName);
+		page.addAttribute("color", randomColor);
 		return "home.html";
 	}
 
