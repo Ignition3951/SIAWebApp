a8d4ae1405392fbd88ff11d048f76a1837a892c7
Utkarsh <ignition3951@gmail.com>
2024-11-27 16:42:54 +0530

    Implement dynamic content response in controller and view

--- src/main/java/com/utk/controller/MainController.java
+++ src/main/java/com/utk/controller/MainController.java
@@ -1,13 +1,16 @@
 package com.utk.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model page) {
+		page.addAttribute("username", "Utkarsh");
+		page.addAttribute("color", "blue");
 		return "home.html";
 	}
 
