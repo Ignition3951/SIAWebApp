c2ec9fdacc0809b17e319c7d54f3bde418b881b9
Utkarsh <ignition3951@gmail.com>
2024-12-05 13:38:03 +0530

    Remove @propertySource annotation to pick properties file directly

--- SiaCommons/src/main/java/com/utk/configuration/ProjectConfig.java
+++ SiaCommons/src/main/java/com/utk/configuration/ProjectConfig.java
@@ -5,12 +5,10 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
-@PropertySource(value = "classpath:application.properties")
 public class ProjectConfig {
 
 	@Value("${custom.datasource.url}")
